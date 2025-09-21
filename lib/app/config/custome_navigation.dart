9db7bc8b1fed195f329800fdbc5a72fa118cd07b
Mahmoud Ashraf <mahmoud.ashraf.elkafrawy@gmail.com>
2025-09-21 12:09:13 +0300

    ss

--- lib/app/config/custome_navigation.dart
+++ lib/app/config/custome_navigation.dart
@@ -4,16 +4,13 @@ void navigateWithAnimation(BuildContext context, Widget page) {
   Navigator.of(context).push(
     PageRouteBuilder(
       transitionDuration: const Duration(milliseconds: 600),
-      reverseTransitionDuration: const Duration(
-        milliseconds: 600,
-      ), // 👈 add this
+      reverseTransitionDuration: const Duration(milliseconds: 600),
       pageBuilder: (context, animation, secondaryAnimation) => page,
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
         final curvedAnimation = CurvedAnimation(
           parent: animation,
           curve: Curves.easeInOut,
         );
-
         return SlideTransition(
           position: Tween<Offset>(
             begin: const Offset(1.0, 0.0), // from right
